4b3e280b4605dcdd44a0eb0e13a2d3f7295ce222
mcris-17 <81688794+mcris-17@users.noreply.github.com>
2021-04-21 01:30:49 +0100

    add server

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jasonServer.defaults();
-const port = process.env.PORT || 3000;
+app.listen(process.env.PORT || 3000);
 
 server.use(middlewares);
 server.use(router);
